1221abafa7e923324ac6bf6b0ba3772617261db2
Aleksandr Astashkin <astashkinav@gmail.com>
2019-07-11 20:02:57 +0400

    test(): refactor test

--- src/utils/norm-random.test.ts
+++ src/utils/norm-random.test.ts
@@ -13,48 +13,44 @@ describe('normRandom', () => {
   }
   const normRandom = new NormRandom();
 
-  describe('plusMinus return valid number', () => {
-    it('return positive number', () => {
-      createMockMath(0.6);
-      expect(normRandom.plusMinus()).toBeGreaterThan(0);
-    });
-    it('return negative number', () => {
-      createMockMath(0.1);
-      expect(normRandom.plusMinus()).toBeLessThan(0);
-    });
-    it('return negative number', () => {
-      createMockMath(0);
-      expect(normRandom.plusMinus()).toBeLessThan(0);
-    });
-    it('return negative number', () => {
-      createMockMath(0.5);
-      expect(normRandom.plusMinus() <= 0).toBeTruthy();
-    });
-    it('return 1', () => {
-      createMockMath(1);
-      expect(normRandom.plusMinus()).toBe(1);
-    });
+  it('return positive number', () => {
+    createMockMath(0.6);
+    expect(normRandom.plusMinus()).toBeGreaterThan(0);
   });
-  describe('zeroTwo return valid number', () => {
-    it('return greater than 1', () => {
-      createMockMath(0.6);
-      expect(normRandom.zeroTwo()).toBeGreaterThan(1);
-    });
-    it('return 0', () => {
-      createMockMath(0);
-      expect(normRandom.zeroTwo()).toBe(0);
-    });
-    it('return 2', () => {
-      createMockMath(1);
-      expect(normRandom.zeroTwo()).toBe(2);
-    });
-    it('return negative number', () => {
-      createMockMath(0.5);
-      expect(normRandom.plusMinus() <= 0).toBeTruthy();
-    });
-    it('return 1', () => {
-      createMockMath(1);
-      expect(normRandom.plusMinus()).toBe(1);
-    });
+  it('return negative number', () => {
+    createMockMath(0.1);
+    expect(normRandom.plusMinus()).toBeLessThan(0);
+  });
+  it('return negative number', () => {
+    createMockMath(0);
+    expect(normRandom.plusMinus()).toBeLessThan(0);
+  });
+  it('return negative number', () => {
+    createMockMath(0.5);
+    expect(normRandom.plusMinus() <= 0).toBeTruthy();
+  });
+  it('return 1', () => {
+    createMockMath(1);
+    expect(normRandom.plusMinus()).toBe(1);
+  });
+  it('return greater than 1', () => {
+    createMockMath(0.51);
+    expect(normRandom.zeroTwo()).toBeGreaterThan(1);
+  });
+  it('return 0', () => {
+    createMockMath(0);
+    expect(normRandom.zeroTwo()).toBe(0);
+  });
+  it('return 2', () => {
+    createMockMath(1);
+    expect(normRandom.zeroTwo()).toBe(2);
+  });
+  it('return negative number', () => {
+    createMockMath(0.5);
+    expect(normRandom.plusMinus() <= 0).toBeTruthy();
+  });
+  it('return 1', () => {
+    createMockMath(1);
+    expect(normRandom.plusMinus()).toBe(1);
   });
 });
